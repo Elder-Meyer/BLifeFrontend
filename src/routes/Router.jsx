8fd703db63fe44909f7eb0fd766db1fd9cd406a0
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-28 07:22:50 -0600

    1.1.4 Add Router to Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Box } from '@mui/material'
 import { Home } from '../views/Home'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import AppNavBar from '../components/AppNavBar'
 
 export const Router = () => {
   return (
-    <Box>
-      <Home/>
-    </Box>
+    <BrowserRouter>
+      <AppNavBar/>
+      <Routes>
+        <Route path="/" element={<Navigate to={"/inicio"}/>} />
+        <Route path="/inicio" element={<Home/>} />
+        <Route path="/Productos" element={"pro"} />
+        <Route path="/Promociones" element={"prom"} />
+        <Route path="/Nosotros" element={"nos"} />
+        <Route path="*" element={"nothing"} />
+      </Routes>
+    </BrowserRouter>
   )
 }
