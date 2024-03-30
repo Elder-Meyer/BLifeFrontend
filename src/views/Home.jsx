580dcca4bcab3b372f505cc296a533553a38d324
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-30 03:43:46 -0600

    3.0.0 Update principal section products Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Box,Card, CardMedia, CardContent, CardActions, Container, Grid, Paper, Button, Stack, Typography, Rating } from "@mui/material";
+import { Box,Card, CardMedia, CardContent, CardActions, Chip, Container, Grid, Paper, Button, Stack, Typography, Rating } from "@mui/material";
 import banner from "../assets/Banner.png";
 import promo from "../assets/promo.png";
 import product from '../assets/Product.png'
-import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Facebook, FavoriteBorderOutlined, Instagram, Star, Twitter } from "@mui/icons-material";
 
 export const Home = () => {
   const numbers = [1, 2, 3, 4];  
@@ -38,14 +38,12 @@ export const Home = () => {
           </Box>
         </Grid>
       </Grid>
-      <br/>
-      <br/>
 
-      <Container maxWidth="xl">
-        <Grid container spacing={3}>
+      <Container maxWidth="xl" sx={{my:6}}>
+        <Grid container spacing={{xs: 0, sm: 3, md: 4, lg: 5}}>
           {
             numbers.map((number) =>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={3} key={number}>
+            <Grid item xs={12} sm={6} md={4} lg={3} xl={3} key={number} sx={{position: "relative"}}>
               {/* <Paper variant="outlined" sx={{m:2, borderRadius: 3, bgcolor: "#f9f9f9"}}>
                 <Paper variant="outlined" sx={{ borderRadius: 3 }}>
                   <Box>
@@ -73,39 +71,54 @@ export const Home = () => {
                   <Button>agregar</Button>
                 </Stack>
               </Paper> */}
-              <Card variant="outlined" sx={{my:2, borderRadius:3, bgcolor: "#f9f9f9"}}>
-                <Paper variant="outlined" sx={{borderRadius:3}}>
+              <Stack spacing={0.5} alignItems={"flex-end"} sx={{position: "absolute", top: { xs: "8%", sm: "12%", md:"16%"}, right: {xs: -9, sm: -13}, zIndex: 3000}}>
+                <Stack direction="row" justifyContent={"space-between"} sx={{width: "62px", height: "31px", borderRadius: "20px", border: "1px solid #e1e1e1", px: "12px", py: 1, bgcolor: "#fff" }}>
+                  <FavoriteBorderOutlined sx={{ width: "14px", height: "14px", color: "#ff8e8e"}} />
+                  <Typography fontWeight={700} fontSize={12} lineHeight={"14.88px"} letterSpacing={"-2%"} color={"#22212c"}>22</Typography>
+                </Stack>
+                <Box sx={{ width: "53px", height: "31px", borderRadius: "20px", border: "1px solid #e1e1e1", px: "12px", py: 1, bgcolor: "#ff8e8e", color: "#fff",  fontWeight: 700, fontSize: "12px", lineHeight: "14.88px", letterSpacing: "-2%" }}>
+                  -30%
+                </Box>
+              </Stack>
+              <Card variant="outlined" sx={{my:2, borderRadius:3, bgcolor: "#f9f9f9", border: "1px solid #E1E1E1"}}>
+                <Paper variant="outlined" sx={{borderRadius:3, border: "1px solid #E1E1E1"}}>
                   <CardMedia
                     component="img"
-                    alt="green iguana"
                     image={product}
-                    // sx={{ height: 300, objectFit: "contain" }}
+                    sx={{ height: 304, objectFit: "contain", mt: 2 }}
                   />
                   <CardContent sx={{px: 3}}>
                     <Stack direction="row" maxWidth={"100%"} justifyContent={"space-between"}>
                       <Box>
-                        <Typography gutterBottom textAlignt="left" fontWeight={700} fontSize={16} color={"#22212c"}>Citrate Mag</Typography>
-                        <Typography textAlignt="left" fontWeight={400} fontSize={16} color={"22212c"}>240 Capsulas | 800 Mg</Typography>
+                        <Typography gutterBottom textAlign="left" fontWeight={700} fontSize={16} lineHeight={"19.84px"} letterSpacing={"-2%"} color={"#22212c"}>Citrate Mag</Typography>
+                        <Typography paragraph textAlign="left" fontWeight={400} fontSize={13} lineHeight={"12.4px"} letterSpacing={"-2%"} color={"22212c"}>240 Capsulas | 800 Mg</Typography>
                       </Box>
                       <Box>
-                        <Typography gutterBottom textAlign="right" fontWeight={700} fontSize={16} color={"#22212c"}>$273.00</Typography>
-                        <Typography textAlign="right" fontWeight={400} fontSize={16} color={"22212c"}><del>$134.00</del></Typography>
+                        <Typography gutterBottom textAlign="right" fontWeight={700} fontSize={16} lineHeight={"19.84px"} letterSpacing={"-2%"} color={"#22212c"}>$273.00</Typography>
+                        <Typography paragraph textAlign="right" fontWeight={400} fontSize={13} lineHeight={"12.4px"} letterSpacing={"-2%"} color={"22212c"}><del>$134.00</del></Typography>
                       </Box>
                     </Stack>
-                    <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly size="small" />
+                    <Rating name="half-rating-read" 
+                      defaultValue={3.5} 
+                      precision={0.5} 
+                      readOnly size="small"
+                      emptyIcon={<Star style={{ color: "#D9D9D9"  }} fontSize="inherit" />}
+                    />
                   </CardContent>
                 </Paper>
-                <CardActions>
+                <CardActions sx={{px: 3, py: 2}}>
                   <Stack direction="row" width={"100%"} justifyContent={"space-between"}>
                     <Box>
-                      <Typography variant="caption" gutterBottom>COMPARTE</Typography>
-                      <Stack direction="row" spacing={0.3}>
-                        <Facebook fontSize="small"/>
-                        <Twitter fontSize="small"/>
-                        <Instagram fontSize="small"/>
+                      <Typography fontWeight={400} fontSize={11} lineHeight={"12.4px"} letterSpacing={"-2%"}>COMPARTE</Typography>
+                      <Stack direction="row" spacing={0.3} width={"100%"} justifyContent={"space-around"}>
+                        <Facebook sx={{width: "15px", height: "15px"}} />
+                        <Twitter sx={{width: "15px", height: "15px"}} />
+                        <Instagram sx={{width: "15px", height: "15px"}} />
                       </Stack>
                     </Box>
-                    <Button sx={{textTransform: "capitalize", px: "22px", py: 1, border: "1px solid #eda836", borderRadius: "20px", bgcolor: "#fff", color: "#22212c", width: "90px", height: "31px"}}>Agregar</Button>
+                    <Button sx={{width: "90px", height: "31px", borderRadius: "20px", border: "1px solid #eda836", px: "22px", py: 1, textTransform: "capitalize",  bgcolor: "#fff", color: "#22212c", fontWeight: 700, fontSize: 12, lineHeight: "14.88px", letterSpacing: "-2%", '&:hover' : {bgcolor: "#eda836"}}}>
+                      Agregar
+                    </Button>
                   </Stack>
                 </CardActions>
               </Card>
