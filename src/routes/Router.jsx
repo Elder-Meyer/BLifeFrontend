8b84e46da7beb2b41cf621f54a5895a82e4b99c0
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-30 05:52:39 -0600

    3.0.5 Update router map to Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Home } from '../views/Home'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppNavBar from '../components/layout/Navbar/AppNavBar'
+import { routeo } from './router.elements'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <AppNavBar/>
       <Routes>
-        <Route path="/" element={<Navigate to={"/inicio"}/>} />
-        <Route path="/inicio" element={<Home/>} />
-        <Route path="/Productos" element={"sección de productos"} />
-        <Route path="/Promociones" element={"sección de promociones"} />
-        <Route path="/Nosotros" element={"sección sobre nosotros"} />
-        <Route path="*" element={"nothing"} />
+        {
+          routeo && routeo.map((route, index)=>{
+            return(
+              <Route key={index} path={route.path} element={route.element} />
+            )
+          })
+        }
       </Routes>
     </BrowserRouter>
   )
